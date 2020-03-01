0c9f1f9aa96f0f4699c3c88f00b44aecfbe4fe81
speedcell4 <speedcell4@gmail.com>
2020-03-01 17:35:20 +0900

    Init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# torch-latent
+
+[![Actions Status](https://github.com/speedcell4/torch-latent/workflows/unit-tests/badge.svg)](https://github.com/speedcell4/torch-latent/actions)
+
+## requirements
+
+- Python3.7 or higher
+- `python3 -m pip install -r requirements.txt`
+
+## latent structures
+
+- [ ] Conditional Random Fields
+- [ ] Non-Projective Dependency Tree
+
+## thanks
+
+this libraries is greatly inspired by [torch-struct](https://github.com/harvardnlp/pytorch-struct)
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setup(
+    name='torch-latent',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/speedcell4/torch-latent',
+    license='MIT',
+    author='speedcell4',
+    author_email='speedcell4@gmail.com',
+    description='A latent structure inference library',
+    long_description=long_description,
+    python_requires='>=3.7',
+)
