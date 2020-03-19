8859b2ce050b3cc4d46e039f083a025ae8398cfa
speedcell4 <speedcell4@gmail.com>
2020-03-19 16:07:00 +0900

    Refactor: Separate BATCH_SIZES, RTOL and ATOL

--- tests/test_semiring.py
+++ tests/test_semiring.py
@@ -3,10 +3,11 @@ from hypothesis import given, strategies as st
 
 from torchlatent.semiring import Std, Log
 
+RTOL = ATOL = 1e-5
+BATCH_SIZES = st.lists(st.integers(1, 12), min_size=1, max_size=5)
 
-@given(
-    batch_sizes=st.lists(st.integers(1, 10), min_size=1, max_size=4),
-)
+
+@given(batch_sizes=BATCH_SIZES)
 def test_add(
         batch_sizes
 ):
@@ -16,12 +17,10 @@ def test_add(
     std = Std.add(lhs.exp(), rhs.exp())
     log = Log.add(lhs, rhs).exp()
 
-    assert torch.allclose(std, log, rtol=1e-5, atol=1e-5)
+    assert torch.allclose(std, log, rtol=RTOL, atol=ATOL)
 
 
-@given(
-    batch_sizes=st.lists(st.integers(1, 10), min_size=1, max_size=4),
-)
+@given(batch_sizes=BATCH_SIZES)
 def test_mul(
         batch_sizes
 ):
@@ -31,12 +30,10 @@ def test_mul(
     std = Std.mul(lhs.exp(), rhs.exp())
     log = Log.mul(lhs, rhs).exp()
 
-    assert torch.allclose(std, log, rtol=1e-5, atol=1e-5)
+    assert torch.allclose(std, log, rtol=RTOL, atol=ATOL)
 
 
-@given(
-    batch_sizes=st.lists(st.integers(1, 10), min_size=1, max_size=4),
-)
+@given(batch_sizes=BATCH_SIZES)
 def test_sum(
         batch_sizes
 ):
@@ -46,12 +43,10 @@ def test_sum(
     std = Std.sum(x.exp(), dim=dim)
     log = Log.sum(x, dim=dim).exp()
 
-    assert torch.allclose(std, log, rtol=1e-5, atol=1e-5)
+    assert torch.allclose(std, log, rtol=RTOL, atol=ATOL)
 
 
-@given(
-    batch_sizes=st.lists(st.integers(1, 10), min_size=1, max_size=4),
-)
+@given(batch_sizes=BATCH_SIZES)
 def test_prod(
         batch_sizes
 ):
@@ -61,4 +56,4 @@ def test_prod(
     std = Std.prod(x.exp(), dim=dim)
     log = Log.prod(x, dim=dim).exp()
 
-    assert torch.allclose(std, log, rtol=1e-5, atol=1e-5)
+    assert torch.allclose(std, log, rtol=RTOL, atol=ATOL)
