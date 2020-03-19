1aad9bd9ad18878b02d784a7790c8099482c558c
speedcell4 <speedcell4@gmail.com>
2020-03-19 16:03:17 +0900

    Test: Add test_mul for Std and Log

--- tests/test_semiring.py
+++ tests/test_semiring.py
@@ -17,3 +17,18 @@ def test_add(
     log = Log.add(lhs, rhs).exp()
 
     assert torch.allclose(std, log, rtol=1e-5, atol=1e-5)
+
+
+@given(
+    batch_sizes=st.lists(st.integers(1, 10), min_size=1, max_size=4),
+)
+def test_mul(
+        batch_sizes
+):
+    lhs = torch.randn(batch_sizes)
+    rhs = torch.randn(batch_sizes)
+
+    std = Std.mul(lhs.exp(), rhs.exp())
+    log = Log.mul(lhs, rhs).exp()
+
+    assert torch.allclose(std, log, rtol=1e-5, atol=1e-5)
