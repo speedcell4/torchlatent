923395dd527cfe29654f926f9e9ace50aabfab6e
speedcell4 <speedcell4@gmail.com>
2020-05-07 20:45:37 +0900

    Refactor: Update BuildCrfInstr

--- torchlatent/proc.py
+++ torchlatent/proc.py
@@ -1,6 +1,7 @@
 import warnings
-from typing import Any
 from typing import List
+
+import torch
 
 from torchlatent.instr import build_crf_instr, collate_crf_instr, BatchInstr, Instr
 
@@ -9,13 +10,20 @@ try:
 
 
     class BuildCrfInstr(Proc):
-        def __call__(self, data: List[Any], *args, **kwargs) -> Instr:
-            return build_crf_instr(length=len(data))
+        def __call__(self, length: int, *args, **kwargs) -> Instr:
+            return build_crf_instr(length=length)
 
 
     class CollateCrfInstr(Proc):
+        def __init__(self, device: torch.device) -> None:
+            super(CollateCrfInstr, self).__init__()
+            self.device = device
+
+        def extra_repr(self) -> str:
+            return f'{self.device}'
+
         def __call__(self, collected_instr: List[Instr], *args, **kwargs) -> BatchInstr:
-            return collate_crf_instr(collected_instr=collected_instr)
+            return collate_crf_instr(collected_instr=collected_instr, device=self.device)
 
 except ImportError:
     warnings.warn(f'torchglyph is required')
