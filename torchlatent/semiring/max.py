45ff11d6642b6fb0bca320334af9a402764cd637
speedcell4 <speedcell4@gmail.com>
2020-10-27 17:02:38 +0900

    Feat: use bym.maxbmm

--- torchlatent/semiring/max.py
+++ torchlatent/semiring/max.py
@@ -1,7 +1,11 @@
+from logging import getLogger
+
 import torch
 from torch import Tensor
 
 from torchlatent.semiring.abc import build_build_unit, build_mv_fn, build_vm_fn, build_mm_fn, build_reduce_fn
+
+logger = getLogger(__name__)
 
 
 def add(lhs: Tensor, rhs: Tensor) -> Tensor:
@@ -26,5 +30,17 @@ build_unit = build_build_unit(zero=zero, one=one)
 
 mv = build_mv_fn(mul_fn=mul, sum_fn=sum)
 vm = build_vm_fn(mul_fn=mul, sum_fn=sum)
-mm = build_mm_fn(mul_fn=mul, sum_fn=sum)
+
+try:
+
+    from bym import maxbmm as mm
+
+    logger.info('using bym.maxbmm')
+
+except ImportError:
+
+    mm = build_mm_fn(mul_fn=mul, sum_fn=sum)
+
+    logger.info('using naive.maxbmm')
+
 reduce = build_reduce_fn(mm_fn=mm)
