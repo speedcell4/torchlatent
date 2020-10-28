d00c2132cc0331a671f524bad2c7f83a31e890a0
speedcell4 <speedcell4@gmail.com>
2020-10-28 15:46:34 +0900

    Refactor: Remove functional.py

--- torchlatent/semiring/log.py
+++ torchlatent/semiring/log.py
@@ -3,10 +3,17 @@ from logging import getLogger
 import torch
 from torch import Tensor
 
-from torchlatent.functional import logsumexp
 from torchlatent.semiring.abc import build_build_unit, build_mv_fn, build_vm_fn, build_mm_fn, build_reduce_fn
 
 logger = getLogger(__name__)
+
+
+def logsumexp(x: Tensor, dim: int) -> Tensor:
+    m, _ = x.max(dim=dim)
+    mask = m == -float('inf')
+
+    s = (x - m.masked_fill_(mask, 0).unsqueeze(dim=dim)).exp().sum(dim=dim)
+    return s.masked_fill_(mask, 1).log() + m.masked_fill_(mask, -float('inf'))
 
 
 def add(lhs: Tensor, rhs: Tensor) -> Tensor:
