d5e7aa0dcf435ee2cdc03ac4a7e04d0e4922ee08
speedcell4 <speedcell4@gmail.com>
2021-01-19 06:13:02 +0900

    Feat: Update logsumexp

--- torchlatent/semiring/log.py
+++ torchlatent/semiring/log.py
@@ -11,8 +11,12 @@ logger = getLogger(__name__)
 def logsumexp(x: Tensor, dim: int) -> Tensor:
     with torch.no_grad():
         m, _ = torch.max(x, dim=dim, keepdim=True)
-    z = (x - m).exp().sum(dim=dim, keepdim=True).log() + m
-    return z.squeeze(dim=dim)
+        m = torch.where(torch.isinf(m), torch.zeros_like(m), m)
+    z = (x - m).exp().sum(dim=dim, keepdim=True)
+    mask = z == 0
+    z = torch.where(mask, torch.ones_like(z), z).log()
+    z = torch.where(mask, torch.full_like(z, -float('inf')), z)
+    return (z + m).squeeze(dim=dim)
 
 
 def add(lhs: Tensor, rhs: Tensor) -> Tensor:
