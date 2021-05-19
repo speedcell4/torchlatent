a7844077b0dce755cc978ddec286ad9b1c45a853
speedcell4 <speedcell4@gmail.com>
2021-05-19 17:33:57 +0900

    Feat: Add CrfDecoderScan

--- torchlatent/crf_scan.py
+++ torchlatent/crf_scan.py
@@ -1,8 +1,15 @@
+from abc import ABCMeta
+from typing import Optional
+
 import torch
 from torch import Tensor
+from torch import nn
+from torch.nn import init
 from torch.nn.utils.rnn import PackedSequence
 from torchrua import reversed_indices, select_last
 
+from torchlatent.crf import compute_log_scores
+from torchlatent.instr import BatchedInstr
 from torchlatent.semiring import log, max
 
 
@@ -100,3 +107,122 @@ def compute_marginals(semiring, scan_semi_scores):
 
 
 compute_log_marginals = compute_marginals(log, scan_log_scores)
+
+
+class CrfDecoderScanABC(nn.Module, metaclass=ABCMeta):
+    def __init__(self, num_tags: int, num_conjugates: int) -> None:
+        super(CrfDecoderScanABC, self).__init__()
+
+        self.num_tags = num_tags
+        self.num_conjugates = num_conjugates
+
+    def reset_parameters(self, bound: float = 0.01) -> None:
+        raise NotImplementedError
+
+    def extra_repr(self) -> str:
+        return ', '.join([
+            f'num_tags={self.num_tags}',
+            f'num_conjugates={self.num_conjugates}',
+        ])
+
+    @staticmethod
+    def _validate(emissions: PackedSequence, tags: Optional[PackedSequence], instr: Optional[BatchedInstr]):
+        return emissions, tags, None
+
+    def obtain_parameters(self, *args, **kwargs):
+        return self.transitions, self.start_transitions, self.end_transitions
+
+    def forward(self, emissions: PackedSequence, tags: Optional[PackedSequence] = None,
+                instr: Optional[BatchedInstr] = None):
+        emissions, tags, instr = self._validate(emissions=emissions, tags=tags, instr=instr)
+        transitions, start_transitions, end_transitions = self.obtain_parameters(
+            emissions=emissions, tags=tags, instr=instr)
+
+        return (emissions, tags, instr), (transitions, start_transitions, end_transitions)
+
+    def fit(self, emissions: PackedSequence, tags: PackedSequence,
+            instr: Optional[BatchedInstr] = None, reduction: str = 'none') -> Tensor:
+        (emissions, tags, instr), (transitions, start_transitions, end_transitions) = self(
+            emissions=emissions, tags=tags, instr=instr)
+
+        log_scores = compute_log_scores(
+            emissions=emissions, tags=tags,
+            transitions=transitions,
+            start_transitions=start_transitions,
+            end_transitions=end_transitions,
+        )
+        log_partitions = scan_log_partitions(
+            emissions=emissions,
+            transitions=transitions,
+            start_transitions=start_transitions,
+            end_transitions=end_transitions,
+        )
+        log_prob = log_scores - log_partitions
+
+        if reduction == 'none':
+            return log_prob
+        if reduction == 'sum':
+            return log_prob.sum()
+        if reduction == 'mean':
+            return log_prob.mean()
+        raise NotImplementedError(f'{reduction} is not supported')
+
+    def decode(self, emissions: PackedSequence, instr: Optional[BatchedInstr] = None) -> PackedSequence:
+        (emissions, _, instr), (transitions, start_transitions, end_transitions) = self(
+            emissions=emissions, tags=None, instr=instr)
+
+        max_partitions = scan_max_partitions(
+            emissions=emissions,
+            transitions=transitions,
+            start_transitions=start_transitions,
+            end_transitions=end_transitions,
+        )
+        predictions = torch.autograd.grad(
+            max_partitions, emissions.data, torch.ones_like(max_partitions),
+            create_graph=False, allow_unused=False, only_inputs=True,
+        )
+
+        return emissions._replace(data=predictions.argmax(dim=-1))
+
+    def marginals(self, emissions: PackedSequence, instr: Optional[BatchedInstr] = None) -> Tensor:
+        (emissions, _, instr), (transitions, start_transitions, end_transitions) = self(
+            emissions=emissions, tags=None, instr=instr)
+
+        return compute_log_marginals(
+            emissions=emissions,
+            transitions=transitions,
+            start_transitions=start_transitions,
+            end_transitions=end_transitions,
+        )
+
+
+class CrfDecoderScan(CrfDecoderScanABC):
+    def __init__(self, num_tags: int, num_conjugates: int = 1) -> None:
+        super(CrfDecoderScan, self).__init__(num_tags=num_tags, num_conjugates=num_conjugates)
+
+        self.transitions = nn.Parameter(
+            torch.empty((1, self.num_conjugates, self.num_tags, self.num_tags)),
+            requires_grad=True,
+        )
+        self.start_transitions = nn.Parameter(
+            torch.empty((1, self.num_conjugates, self.num_tags)),
+            requires_grad=True,
+        )
+        self.end_transitions = nn.Parameter(
+            torch.empty((1, self.num_conjugates, self.num_tags)),
+            requires_grad=True,
+        )
+
+        self.reset_parameters()
+
+    @torch.no_grad()
+    def reset_parameters(self, bound: float = 0.01) -> None:
+        init.uniform_(self.transitions, -bound, +bound)
+        init.uniform_(self.start_transitions, -bound, +bound)
+        init.uniform_(self.end_transitions, -bound, +bound)
+
+    def extra_repr(self) -> str:
+        return ', '.join([
+            f'num_tags={self.num_tags}',
+            f'num_conjugates={self.num_conjugates}',
+        ])
