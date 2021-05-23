56c7f4785132d730df381a0ed31231f6c44bb478
speedcell4 <speedcell4@gmail.com>
2021-05-23 15:49:47 +0900

    Feat: Add CrfDecoderScanABC

--- torchlatent/__init__.py
+++ torchlatent/__init__.py
@@ -1,5 +1,8 @@
 from torchlatent.crf import CrfDistribution, CrfDecoderABC, CrfDecoder, ConjugatedCrfDecoder
+from torchlatent.crf_scan import CrfDecoderScanABC, CrfDecoderScan
 
 __all__ = [
-    'CrfDistribution', 'CrfDecoderABC', 'CrfDecoder', 'ConjugatedCrfDecoder',
+    'CrfDistribution',
+    'CrfDecoderABC', 'CrfDecoder', 'ConjugatedCrfDecoder',
+    'CrfDecoderScanABC', 'CrfDecoderScan',
 ]
