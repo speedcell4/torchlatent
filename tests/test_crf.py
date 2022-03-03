efec2a5c1f8b453dd0ee84517bddb3d5c0e77747
speedcell4 <speedcell4@gmail.com>
2022-03-04 00:30:12 +0900

    Test: Add sizes

--- tests/test_crf.py
+++ tests/test_crf.py
@@ -2,17 +2,17 @@ import torch
 from hypothesis import given
 from torchrua import pack_sequence, cat_sequence, pack_catted_sequence
 
-from tests.strategies import devices, token_size_lists, conjugate_sizes, tag_sizes
+from tests.strategies import devices, sizes, BATCH_SIZE, TOKEN_SIZE, NUM_CONJUGATES, NUM_TAGS
 from tests.third_party import ThirdPartyCrfDecoder
-from tests.utils import assert_close, assert_grad_close, assert_packed_equal
+from tests.utils import assert_close, assert_grad_close, assert_packed_sequence_equal
 from torchlatent.crf import CrfDecoder
 
 
 @given(
     device=devices(),
-    token_sizes=token_size_lists(),
-    num_conjugate=conjugate_sizes(),
-    num_tags=tag_sizes(),
+    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
+    num_conjugate=sizes(NUM_CONJUGATES),
+    num_tags=sizes(NUM_TAGS),
 )
 def test_crf_packed_fit(device, token_sizes, num_conjugate, num_tags):
     emissions = pack_sequence([
@@ -25,8 +25,8 @@ def test_crf_packed_fit(device, token_sizes, num_conjugate, num_tags):
         for token_size in token_sizes
     ], device=device)
 
-    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
-    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
+    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
+    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
     expected_decoder.reset_parameters_with_(decoder=actual_decoder)
 
     actual = actual_decoder.fit(emissions=emissions, tags=tags)
@@ -38,9 +38,9 @@ def test_crf_packed_fit(device, token_sizes, num_conjugate, num_tags):
 
 @given(
     device=devices(),
-    token_sizes=token_size_lists(),
-    num_conjugate=conjugate_sizes(),
-    num_tags=tag_sizes(),
+    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
+    num_conjugate=sizes(NUM_CONJUGATES),
+    num_tags=sizes(NUM_TAGS),
 )
 def test_crf_packed_decode(device, token_sizes, num_conjugate, num_tags):
     emissions = pack_sequence([
@@ -48,21 +48,21 @@ def test_crf_packed_decode(device, token_sizes, num_conjugate, num_tags):
         for token_size in token_sizes
     ], device=device)
 
-    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
-    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
+    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
+    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
     expected_decoder.reset_parameters_with_(decoder=actual_decoder)
 
     expected = expected_decoder.decode(emissions=emissions)
     actual = actual_decoder.decode(emissions=emissions)
 
-    assert_packed_equal(actual=actual, expected=expected)
+    assert_packed_sequence_equal(actual=actual, expected=expected)
 
 
 @given(
     device=devices(),
-    token_sizes=token_size_lists(),
-    num_conjugate=conjugate_sizes(),
-    num_tags=tag_sizes(),
+    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
+    num_conjugate=sizes(NUM_CONJUGATES),
+    num_tags=sizes(NUM_TAGS),
 )
 def test_crf_catted_fit(device, token_sizes, num_conjugate, num_tags):
     emissions = [
@@ -80,8 +80,8 @@ def test_crf_catted_fit(device, token_sizes, num_conjugate, num_tags):
     catted_tags = cat_sequence(tags, device=device)
     packed_tags = pack_sequence(tags, device=device)
 
-    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
-    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
+    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
+    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
     expected_decoder.reset_parameters_with_(decoder=actual_decoder)
 
     actual = actual_decoder.fit(emissions=catted_emissions, tags=catted_tags)
@@ -93,9 +93,9 @@ def test_crf_catted_fit(device, token_sizes, num_conjugate, num_tags):
 
 @given(
     device=devices(),
-    token_sizes=token_size_lists(),
-    num_conjugate=conjugate_sizes(),
-    num_tags=tag_sizes(),
+    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
+    num_conjugate=sizes(NUM_CONJUGATES),
+    num_tags=sizes(NUM_TAGS),
 )
 def test_crf_catted_decode(device, token_sizes, num_conjugate, num_tags):
     emissions = [
@@ -106,12 +106,12 @@ def test_crf_catted_decode(device, token_sizes, num_conjugate, num_tags):
     catted_emissions = cat_sequence(emissions, device=device)
     packed_emissions = pack_sequence(emissions, device=device)
 
-    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
-    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate)
+    actual_decoder = CrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
+    expected_decoder = ThirdPartyCrfDecoder(num_tags=num_tags, num_conjugates=num_conjugate).to(device=device)
     expected_decoder.reset_parameters_with_(decoder=actual_decoder)
 
     expected = expected_decoder.decode(emissions=packed_emissions)
     actual = actual_decoder.decode(emissions=catted_emissions)
     actual = pack_catted_sequence(*actual, device=device)
 
-    assert_packed_equal(actual=actual, expected=expected)
+    assert_packed_sequence_equal(actual=actual, expected=expected)
--- tests/utils.py
+++ tests/utils.py
@@ -1,39 +1,81 @@
-from typing import Tuple, List, Union
+from typing import List, Tuple, Union
 
 import torch
 from torch import Tensor
+from torch.nn.utils.rnn import PackedSequence
 from torch.testing import assert_close
-from torchrua import CattedSequence, PackedSequence
+from torchrua.catting import CattedSequence
 
 __all__ = [
-    'assert_close',
-    'assert_equal',
-    'assert_grad_close',
-    'assert_packed_close',
-    'assert_packed_equal',
-    'assert_catted_close',
-    'assert_catted_equal',
+    'assert_equal', 'assert_close', 'assert_grad_close',
+    'assert_catted_sequence_equal', 'assert_catted_sequence_close',
+    'assert_packed_sequence_equal', 'assert_packed_sequence_close',
 ]
 
 
-def assert_equal(actual: Tensor, expected: Tensor, **kwargs) -> None:
+def assert_equal(actual: Tensor, expected: Tensor) -> None:
     assert torch.equal(actual, expected)
 
 
-def assert_grad_close(actual: Tensor, expected: Tensor, inputs: Union[List[Tensor], Tuple[Tensor, ...]]) -> None:
-    grad = torch.randn_like(actual)
+def assert_grad_close(
+        actual: Tensor, expected: Tensor,
+        inputs: Union[Tensor, List[Tensor], Tuple[Tensor, ...]],
+        allow_unused: bool = False,
+        check_device: bool = True, check_dtype: bool = True, check_stride: bool = True) -> None:
+    kwargs = dict(check_device=check_device, check_dtype=check_dtype, check_stride=check_stride)
 
-    actual_grads = torch.autograd.grad(actual, inputs, grad)
-    expected_grads = torch.autograd.grad(expected, inputs, grad)
+    grad = torch.rand_like(actual)
+
+    actual_grads = torch.autograd.grad(
+        actual, inputs, grad,
+        create_graph=False,
+        allow_unused=allow_unused,
+    )
+
+    expected_grads = torch.autograd.grad(
+        expected, inputs, grad,
+        create_graph=False,
+        allow_unused=allow_unused,
+    )
 
     for actual_grad, expected_grad in zip(actual_grads, expected_grads):
-        if actual_grad is None:
-            assert expected_grad is None
-        else:
-            assert_close(actual=actual_grad, expected=expected_grad)
+        assert_close(actual=actual_grad, expected=expected_grad, **kwargs)
 
 
-def assert_packed_equal(actual: PackedSequence, expected: PackedSequence) -> None:
+def assert_catted_sequence_close(
+        actual: CattedSequence, expected: CattedSequence,
+        check_device: bool = True, check_dtype: bool = True, check_stride: bool = True) -> None:
+    kwargs = dict(check_device=check_device, check_dtype=check_dtype, check_stride=check_stride)
+
+    assert_close(actual=actual.data, expected=expected.data, **kwargs)
+    assert_equal(actual=actual.token_sizes, expected=expected.token_sizes)
+
+
+def assert_catted_sequence_equal(actual: CattedSequence, expected: CattedSequence) -> None:
+    assert_equal(actual=actual.data, expected=expected.data)
+    assert_equal(actual=actual.token_sizes, expected=expected.token_sizes)
+
+
+def assert_packed_sequence_close(
+        actual: PackedSequence, expected: PackedSequence,
+        check_device: bool = True, check_dtype: bool = True, check_stride: bool = True) -> None:
+    kwargs = dict(check_device=check_device, check_dtype=check_dtype, check_stride=check_stride)
+
+    assert_close(actual=actual.data, expected=expected.data, **kwargs)
+    assert_equal(actual=actual.batch_sizes, expected=expected.batch_sizes)
+
+    if actual.sorted_indices is None:
+        assert expected.sorted_indices is None
+    else:
+        assert_equal(actual=actual.sorted_indices, expected=expected.sorted_indices)
+
+    if actual.unsorted_indices is None:
+        assert expected.unsorted_indices is None
+    else:
+        assert_equal(actual=actual.unsorted_indices, expected=expected.unsorted_indices)
+
+
+def assert_packed_sequence_equal(actual: PackedSequence, expected: PackedSequence) -> None:
     assert_equal(actual=actual.data, expected=expected.data)
     assert_equal(actual=actual.batch_sizes, expected=expected.batch_sizes)
 
@@ -46,28 +88,3 @@ def assert_packed_equal(actual: PackedSequence, expected: PackedSequence) -> Non
         assert expected.unsorted_indices is None
     else:
         assert_equal(actual=actual.unsorted_indices, expected=expected.unsorted_indices)
-
-
-def assert_packed_close(actual: PackedSequence, expected: PackedSequence) -> None:
-    assert_close(actual=actual.data, expected=expected.data)
-    assert_equal(actual=actual.batch_sizes, expected=expected.batch_sizes)
-
-    if actual.sorted_indices is None:
-        assert expected.sorted_indices is None
-    else:
-        assert_equal(actual=actual.sorted_indices, expected=expected.sorted_indices)
-
-    if actual.unsorted_indices is None:
-        assert expected.unsorted_indices is None
-    else:
-        assert_equal(actual=actual.unsorted_indices, expected=expected.unsorted_indices)
-
-
-def assert_catted_equal(actual: CattedSequence, expected: CattedSequence) -> None:
-    assert_equal(actual.data, expected.data)
-    assert_equal(actual.token_sizes, expected.token_sizes)
-
-
-def assert_catted_close(actual: CattedSequence, expected: CattedSequence) -> None:
-    assert_close(actual.data, expected.data)
-    assert_close(actual.token_sizes, expected.token_sizes)
