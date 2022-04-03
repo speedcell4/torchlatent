576bcec45d18359de2d69ef92eeeb72c73dad918
speedcell4 <speedcell4@gmail.com>
2022-04-03 16:12:54 +0900

    Test: Add unit test, test_cky_catted_max

--- tests/test_cky.py
+++ tests/test_cky.py
@@ -2,11 +2,30 @@ import torch
 from hypothesis import given, strategies as st
 from torch.testing import assert_close
 from torch_struct import TreeCRF
-from torchrua import pack_sequence
+from torchrua import pack_sequence, cat_sequence
 
 from tests.strategies import sizes, BATCH_SIZE, TOKEN_SIZE, EMBEDDING_DIM, devices, TINY_BATCH_SIZE
 from tests.utils import assert_grad_close
 from torchlatent.cky import CkyDistribution, cky_indices, CkyDecoder
+
+
+@given(
+    device=devices(),
+    token_sizes=sizes(TINY_BATCH_SIZE, TOKEN_SIZE),
+    embedding_dim=sizes(EMBEDDING_DIM),
+    num_tags=sizes(TOKEN_SIZE),
+    bias=st.booleans(),
+)
+def test_cky_catted_max(device, token_sizes, embedding_dim, num_tags, bias):
+    sequence = cat_sequence([
+        torch.randn((token_size, embedding_dim), requires_grad=True, device=device)
+        for token_size in token_sizes
+    ])
+
+    decoder = CkyDecoder(in_features=embedding_dim, out_features=num_tags, bias=bias)
+    cky = decoder.forward(sequence=sequence)
+
+    assert_close(actual=cky.max, expected=cky.log_scores(decoder.decode(sequence=sequence)))
 
 
 @given(
@@ -44,4 +63,4 @@ def test_cky_log_partitions(device, token_sizes, num_tags):
     actual = CkyDistribution(scores=scores, indices=cky_indices(token_sizes=token_sizes, device=device))
 
     assert_close(actual=actual.log_partitions, expected=excepted.partition)
-    assert_grad_close(actual=actual.log_partitions, expected=excepted.partition, inputs=scores)
+    assert_grad_close(actual=actual.log_partitions, expected=excepted.partition, inputs=scores, rtol=1e-5, atol=1e-5)
