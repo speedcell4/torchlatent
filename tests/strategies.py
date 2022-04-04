2fb08427a18c8a48a0c64b49c199a6e040e7a9f4
speedcell4 <speedcell4@gmail.com>
2022-04-04 09:26:15 +0900

    Test: Update strategies

--- tests/strategies.py
+++ tests/strategies.py
@@ -2,15 +2,17 @@ import torch
 
 from hypothesis import strategies as st
 
-TINY_BATCH_SIZE = 6
-TINY_TOKEN_SIZE = 12
-
-BATCH_SIZE = 24
+BATCH_SIZE = 25
 TOKEN_SIZE = 50
-NUM_TAGS = 8
 NUM_CONJUGATES = 5
+NUM_TAGS = 15
+EMBEDDING_DIM = 16
 
-EMBEDDING_DIM = 25
+TINY_BATCH_SIZE = 5
+TINY_TOKEN_SIZE = 10
+TINY_NUM_CONJUGATES = 3
+TINY_NUM_TAGS = 3
+TINY_EMBEDDING_DIM = 4
 
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
@@ -20,13 +22,10 @@ _ = torch.empty((1,), device=device)
 
 
 @st.composite
-def sizes(draw, *size: int, min_size: int = 1):
-    max_size, *size = size
+def sizes(draw, *max_sizes: int, min_size: int = 1):
+    max_size, *max_sizes = max_sizes
+    n = draw(st.integers(min_value=min_size, max_value=max_size))
 
-    if len(size) == 0:
-        return draw(st.integers(min_value=min_size, max_value=max_size))
-    else:
-        return [
-            draw(sizes(*size, min_size=min_size))
-            for _ in range(draw(st.integers(min_value=min_size, max_value=max_size)))
-        ]
+    if len(max_sizes) == 0:
+        return n
+    return [draw(sizes(*max_sizes, min_size=min_size)) for _ in range(n)]
