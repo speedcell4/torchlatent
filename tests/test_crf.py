50b2a871244729cf52dda168ca613d113c894704
speedcell4 <speedcell4@gmail.com>
2022-04-07 20:40:43 +0900

    Refactor: Add test_crf_catted_fit and test_crf_packed_fit

--- tests/test_crf.py
+++ tests/test_crf.py
@@ -2,12 +2,57 @@ import torch
 import torchcrf
 from hypothesis import given
 from torch.testing import assert_close
-from torchrua import cat_sequence, pad_catted_indices, pack_catted_indices
-from torchrua import pad_sequence, pad_packed_indices, pack_sequence
 
 from tests.strategies import device, sizes, TOKEN_SIZE, TINY_BATCH_SIZE, NUM_CONJUGATES, TINY_TOKEN_SIZE
 from tests.utils import assert_grad_close, assert_equal
 from torchlatent.crf import CrfDecoder
+from torchrua import cat_sequence, pad_catted_indices, pack_catted_indices
+from torchrua import pad_sequence, pad_packed_indices, pack_sequence
+
+
+@given(
+    token_sizes=sizes(TINY_BATCH_SIZE, TOKEN_SIZE),
+    num_tags=sizes(TOKEN_SIZE),
+)
+def test_crf_catted_scores(token_sizes, num_tags):
+    actual_decoder = CrfDecoder(num_tags)
+    excepted_decoder = torchcrf.CRF(num_tags, batch_first=False)
+
+    excepted_decoder.transitions.data = torch.randn_like(excepted_decoder.transitions)
+    excepted_decoder.start_transitions.data = torch.randn_like(excepted_decoder.start_transitions)
+    excepted_decoder.end_transitions.data = torch.randn_like(excepted_decoder.end_transitions)
+
+    actual_decoder.transitions.data = excepted_decoder.transitions[None, None, :, :]
+    actual_decoder.head_transitions.data = excepted_decoder.start_transitions[None, None, :]
+    actual_decoder.last_transitions.data = excepted_decoder.end_transitions[None, None, :]
+
+    emissions = [
+        torch.randn((token_size, num_tags), requires_grad=True, device=device)
+        for token_size in token_sizes
+    ]
+    targets = [
+        torch.randint(0, num_tags, (token_size,), device=device)
+        for token_size in token_sizes
+    ]
+
+    catted_emissions = cat_sequence([x[:, None] for x in emissions])
+    catted_targets = cat_sequence([x[:, None] for x in targets])
+
+    padded_emissions, _ = pad_sequence(emissions, batch_first=False)
+    padded_targets, _ = pad_sequence(targets, batch_first=False)
+
+    size, ptr = pad_catted_indices(token_sizes=catted_emissions.token_sizes, batch_first=False)
+    mask = torch.zeros(size, dtype=torch.bool, device=device)
+    mask[ptr] = True
+
+    actual = actual_decoder.forward(emissions=catted_emissions).log_scores(targets=catted_targets)[:, 0]
+    excepted = excepted_decoder._compute_score(
+        emissions=padded_emissions, tags=padded_targets.long(),
+        mask=mask.byte(),
+    )
+
+    assert_close(actual=actual, expected=excepted, rtol=1e-4, atol=1e-4)
+    assert_grad_close(actual=actual, expected=excepted, inputs=emissions, rtol=1e-4, atol=1e-4)
 
 
 @given(
@@ -91,6 +136,56 @@ def test_crf_catted_decode(token_sizes, num_tags):
 
     assert_equal(actual=actual[:, 0], expected=excepted)
     assert_equal(actual=actual_token_sizes, expected=excepted_token_sizes)
+
+
+@given(
+    token_sizes=sizes(TINY_BATCH_SIZE, TOKEN_SIZE),
+    num_tags=sizes(TOKEN_SIZE),
+)
+def test_crf_packed_scores(token_sizes, num_tags):
+    actual_decoder = CrfDecoder(num_tags)
+    excepted_decoder = torchcrf.CRF(num_tags, batch_first=False)
+
+    excepted_decoder.transitions.data = torch.randn_like(excepted_decoder.transitions)
+    excepted_decoder.start_transitions.data = torch.randn_like(excepted_decoder.start_transitions)
+    excepted_decoder.end_transitions.data = torch.randn_like(excepted_decoder.end_transitions)
+
+    actual_decoder.transitions.data = excepted_decoder.transitions[None, None, :, :]
+    actual_decoder.head_transitions.data = excepted_decoder.start_transitions[None, None, :]
+    actual_decoder.last_transitions.data = excepted_decoder.end_transitions[None, None, :]
+
+    emissions = [
+        torch.randn((token_size, num_tags), requires_grad=True, device=device)
+        for token_size in token_sizes
+    ]
+    targets = [
+        torch.randint(0, num_tags, (token_size,), device=device)
+        for token_size in token_sizes
+    ]
+
+    packed_emissions = pack_sequence([x[:, None] for x in emissions])
+    packed_targets = pack_sequence([x[:, None] for x in targets])
+
+    padded_emissions, _ = pad_sequence(emissions, batch_first=False)
+    padded_targets, _ = pad_sequence(targets, batch_first=False)
+
+    size, ptr, _ = pad_packed_indices(
+        batch_sizes=packed_emissions.batch_sizes,
+        sorted_indices=packed_emissions.sorted_indices,
+        unsorted_indices=packed_emissions.unsorted_indices,
+        batch_first=False,
+    )
+    mask = torch.zeros(size, dtype=torch.bool, device=device)
+    mask[ptr] = True
+
+    actual = actual_decoder.forward(emissions=packed_emissions).log_scores(targets=packed_targets)[:, 0]
+    excepted = excepted_decoder._compute_score(
+        emissions=padded_emissions, tags=padded_targets.long(),
+        mask=mask.byte(),
+    )
+
+    assert_close(actual=actual, expected=excepted, rtol=1e-4, atol=1e-4)
+    assert_grad_close(actual=actual, expected=excepted, inputs=emissions, rtol=1e-4, atol=1e-4)
 
 
 @given(
