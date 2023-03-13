80c4727da24f230236c23d3a045d66241ea21654
speedcell4 <speedcell4@gmail.com>
2023-03-13 23:44:10 +0900

    Refactor: Remove num_conjugates from BiaffineClassifier

--- torchlatent/nn/classifier.py
+++ torchlatent/nn/classifier.py
@@ -37,19 +37,18 @@ class Classifier(nn.Module):
 
 
 class BiaffineClassifier(nn.Module):
-    def __init__(self, bias: bool = False, *, num_conjugates: int,
+    def __init__(self, bias: bool = False, *,
                  in_features1: int, in_features2: int, out_features: int) -> None:
         super(BiaffineClassifier, self).__init__()
 
         self.in_features1 = in_features1
         self.in_features2 = in_features2
         self.out_features = out_features
-        self.num_conjugates = num_conjugates
 
-        self.weight0 = nn.Parameter(torch.empty((num_conjugates, out_features, in_features1, in_features2)))
-        self.weight1 = nn.Parameter(torch.empty((num_conjugates, out_features, in_features1)))
-        self.weight2 = nn.Parameter(torch.empty((num_conjugates, out_features, in_features2)))
-        self.bias = nn.Parameter(torch.empty((num_conjugates, out_features))) if bias else 0
+        self.weight0 = nn.Parameter(torch.empty((out_features, in_features1, in_features2)))
+        self.weight1 = nn.Parameter(torch.empty((out_features, in_features1)))
+        self.weight2 = nn.Parameter(torch.empty((out_features, in_features2)))
+        self.bias = nn.Parameter(torch.empty((out_features,))) if bias else 0
 
         self.reset_parameters()
 
@@ -66,13 +65,12 @@ class BiaffineClassifier(nn.Module):
             f'in_features1={self.in_features1}',
             f'in_features2={self.in_features2}',
             f'out_features={self.out_features}',
-            f'num_conjugates={self.num_conjugates}',
             f'bias={torch.is_tensor(self.bias)}',
         ])
 
     def forward(self, tensor1: Tensor, tensor2: Tensor) -> Tensor:
-        tensor0 = torch.einsum('nzxy,...nx,...ny->...nz', self.weight0, tensor1, tensor2)
-        tensor1 = torch.einsum('nzx,...nx->...nz', self.weight1, tensor1)
-        tensor2 = torch.einsum('nzy,...ny->...nz', self.weight2, tensor2)
+        tensor0 = torch.einsum('zxy,...x,...y->...z', self.weight0, tensor1, tensor2)
+        tensor1 = torch.einsum('zx,...x->...z', self.weight1, tensor1)
+        tensor2 = torch.einsum('zy,...y->...z', self.weight2, tensor2)
 
         return tensor0 + tensor1 + tensor2 + self.bias
