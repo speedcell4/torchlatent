f5d3f83b78b2ba4e31cc78c8a0891bd526893af7
speedcell4 <speedcell4@gmail.com>
2023-08-13 19:31:56 +0900

    Feat: Add abc2.py

---
+++ torchlatent/abc2.py
@@ -0,0 +1,63 @@
+from abc import ABCMeta
+from typing import Tuple
+from typing import Union
+
+import torch
+import torch.autograd
+from torch import Tensor
+from torch.distributions.utils import lazy_property
+from torch.nn.utils.rnn import PackedSequence
+from torchrua import CattedSequence
+
+Sequence = Union[CattedSequence, PackedSequence, Tuple[Tensor, Tensor]]
+
+
+class StructuredDistribution(object, metaclass=ABCMeta):
+    @property
+    def emissions(self) -> Sequence:
+        raise NotImplementedError
+
+    @lazy_property
+    def log_emissions(self) -> Sequence:
+        return self.emissions
+
+    @lazy_property
+    def max_emissions(self) -> Sequence:
+        return self.emissions
+
+    def log_scores(self, targets: Sequence) -> Tensor:
+        raise NotImplementedError
+
+    def log_prob(self, targets: Sequence) -> Tensor:
+        return self.log_scores(targets=targets) - self.log_partitions
+
+    @lazy_property
+    def log_partitions(self) -> Tensor:
+        raise NotImplementedError
+
+    @lazy_property
+    def marginals(self) -> Tensor:
+        emissions, *_ = self.log_emissions
+        grad, = torch.autograd.grad(
+            self.log_partitions, emissions, torch.ones_like(self.log_partitions),
+            create_graph=True, retain_graph=True, only_inputs=True, allow_unused=True,
+
+        )
+        return grad
+
+    @lazy_property
+    def max(self) -> Tensor:
+        raise NotImplementedError
+
+    @lazy_property
+    def argmax(self) -> Tensor:
+        emissions, *_ = self.max_emissions
+        grad, = torch.autograd.grad(
+            self.max, emissions, torch.ones_like(self.max),
+            create_graph=False, retain_graph=False, only_inputs=True, allow_unused=True,
+        )
+        return grad
+
+    @lazy_property
+    def entropy(self) -> Tensor:
+        raise NotImplementedError
