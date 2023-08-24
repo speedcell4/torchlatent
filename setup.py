66a86294a1e374e82f3f967ed30e00116f7bf87a
speedcell4 <speedcell4@gmail.com>
2023-08-24 19:55:26 +0900

    Chore: Upgrade setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
 name = 'torchlatent'
 
+root_dir = Path(__file__).parent.resolve()
+with (root_dir / 'requirements.txt').open(mode='r', encoding='utf-8') as fp:
+    install_requires = [install_require.strip() for install_require in fp]
+
 setup(
     name=name,
-    version='0.4.2',
+    version='0.5.0',
     packages=[package for package in find_packages() if package.startswith(name)],
     url='https://github.com/speedcell4/torchlatent',
     license='MIT',
     author='speedcell4',
     author_email='speedcell4@gmail.com',
     description='High Performance Structured Prediction in PyTorch',
-    python_requires='>=3.9',
-    install_requires=[
-        'numpy',
-        'torchrua',
-    ],
+    python_requires='>=3.8',
+    install_requires=install_requires,
 )
